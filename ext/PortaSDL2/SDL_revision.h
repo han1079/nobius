e9ed2072e736bc37141622d25807d089ce1f4637
Han Zhang <azure.han.zhang@gmail.com>
2025-10-28 15:57:37 -0700

    CMake now builds a local version of SDL2 in ext/PortaSDL2

---
+++ ext/PortaSDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.32.10-16-gc0d0b31d1 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.32.10-16-gc0d0b31d1"
+#endif
